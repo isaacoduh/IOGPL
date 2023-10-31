3dc8e7454eaeaacc782d8f36517efae045c12261
isaacoduh <ufuomaoduh@gmail.com>
2023-10-31 21:42:12 +0000

    Setup Command Abstract Class.

---
+++ IOGPL/Command.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOGPL
+{
+    /// <summary>
+    /// Abstract Class that defines a common method Execute. That 
+    /// is executed by all basic command classes
+    /// </summary>
+    public abstract class Command
+    {
+        public abstract void Execute();
+    }
+}
