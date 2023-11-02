4cffdc692dde5c8542c0dc21ce97885356f73be4
isaacoduh <ufuomaoduh@gmail.com>
2023-11-02 14:19:46 +0000

    Create a CommandParser class

---
+++ IOGPL/CommandParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOGPL
+{
+    public class CommandParser
+    {
+        public string Action { get; set; }
+        public string[] Tokens { get; set; }
+
+        public void Parse(string command)
+        {
+            string[] parts = command.Split(' ');
+            if (parts.Length == 2)
+            {
+                Action = parts[0];
+                Tokens = parts[1].Split(',');
+            }
+
+        }
+    }
+}
