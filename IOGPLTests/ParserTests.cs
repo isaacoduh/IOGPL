a71ef0b25d3728d0e5ed59edba7cecc39394d4b4
isaacoduh <ufuomaoduh@gmail.com>
2023-11-04 08:58:02 +0000

    Add Unit tests to valid draw command with command parser.

--- IOGPLTests/ParserTests.cs
+++ IOGPLTests/ParserTests.cs
@@ -30,5 +30,50 @@ namespace IOGPLTests
             // Act
             parser.Parse("movto 100,100");
         }
+
+
+        [TestMethod]
+        public void Parse_ValidDrawToCommand()
+        {
+            // arrange
+            var parser = new CommandParser();
+
+            // act
+            parser.Parse("drawto 200,100");
+
+            Assert.AreEqual("drawto", parser.Action);
+            CollectionAssert.AreEqual(new string[] {"200","100"}, parser.Tokens);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidCommandException))]
+        public void Parse_InvalidDrawToCommand()
+        {
+            var parser = new CommandParser();
+
+            parser.Parse("drawwto 200,200");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidTokenCountException))]
+        public void Parse_InvalidTokenCountDrawCommand_With_One_Token()
+        {
+            // arrange
+            var parser = new CommandParser();
+
+            // act
+            parser.Parse("drawto 100");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidTokenCountException))]
+        public void Parse_InvalidTokenCountDrawCommand_With_Three_Tokens()
+        {
+            // arrange
+            var parser = new CommandParser();
+
+            // act
+            parser.Parse("drawto 100,100,100");
+        }
     }
 }
