a9d2e66d95f18dad0aeedbf3e9ae81fc65bdbca9
isaacoduh <ufuomaoduh@gmail.com>
2023-12-14 12:41:22 +0000

    create a prototype class to build out the entire program.

---
+++ IOGPL/Prototype.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOGPL
+{
+    public class Proto
+    {
+        /// <summary>
+        /// This class will be the sandbox for the nextpart of the application
+        /// </summary>
+        public Proto() { }
+
+        public void processProgram(string[] program) 
+        { 
+            foreach(string i in program)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
