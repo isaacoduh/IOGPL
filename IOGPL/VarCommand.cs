47dd7a89fc6feecc4d314522ecd3632c59468f1d
isaacoduh <ufuomaoduh@gmail.com>
2023-12-24 00:00:10 +0000

    XML for Tests and Variable Command Class.

--- IOGPL/VarCommand.cs
+++ IOGPL/VarCommand.cs
@@ -7,18 +7,43 @@ using System.Threading.Tasks;
 
 namespace IOGPL
 {
+    /// <summary>
+    /// Represents a command handler for variable-related operations in a custom programming language.
+    /// </summary>
     public class VarCommand
     {
         private string[] parameters;
         private string valString;
         private int Val;
 
+        /// <summary>
+        /// Default constructor for the VarCommand class.
+        /// </summary>
+
         public VarCommand() { }
+
+
+        /// <summary>
+        /// Constructor that takes a BaseCanvas parameter. (Note: The purpose of this constructor is unclear from the provided code.)
+        /// </summary>
+        /// <param name="c">An instance of the BaseCanvas class.</param>
+
         public VarCommand(BaseCanvas c) { }
         public VarCommand(string[] body)
         {
             this.parameters = body;
         }
+
+
+
+        /// <summary>
+        /// Handles variable-related operations based on the provided parameters.
+        /// </summary>
+        /// <param name="parameters">An array of strings representing the parameters of the command.</param>
+        /// <param name="variables">A dictionary representing the current state of variables.</param>
+        /// <param name="variableCounter">A reference to the variable counter.</param>
+        /// <param name="reassignment">A flag indicating whether the operation is a variable reassignment.</param>
+
 
         public void Handle(string[] parameters, Dictionary<string, int> variables, ref int variableCounter, bool reassignment = false)
         {
@@ -119,6 +144,14 @@ namespace IOGPL
             }
         }
 
+        /// <summary>
+        /// Declares a variable and sets its initial value.
+        /// </summary>
+        /// <param name="variables">A dictionary representing the current state of variables.</param>
+        /// <param name="variableCounter">A reference to the variable counter.</param>
+        /// <param name="varName">The name of the variable to declare.</param>
+        /// <param name="varValue">The initial value of the variable.</param>
+
         public void DeclareVariable(Dictionary<string, int> variables, ref int variableCounter, string varName, string varValue)
         {
             if (!variables.ContainsKey(varName))
@@ -150,10 +183,23 @@ namespace IOGPL
             }
         }
 
+
+        /// <summary>
+        /// Sets the value of a variable.
+        /// </summary>
+        /// <param name="varName">The name of the variable to set.</param>
+        /// <param name="varValue">The new value of the variable.</param>
+
         public void SetVariable(string varName, string varValue)
         {
             Console.WriteLine($"Setting...: {varName} = {varValue}");
         }
+
+
+        /// <summary>
+        /// Checks if a variable with the specified name is already declared.
+        /// </summary>
+        /// <param name="varName">The name of the variable to check.</param>
 
         public void checkVariable(string varName)
         {
---
+++ IOGPLTests/VarCommandTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IOGPL;
+
+namespace IOGPLTests
+{
+    /// <summary>
+    /// Represents a test class for the VarCommand class.
+    /// </summary>
+    [TestClass]
+    public class VarCommandTests
+    {
+        /// <summary>
+        /// Tests the DeclareVariable method when the variable is not declared.
+        /// </summary>
+        [TestMethod]
+        public void DeclareVariable_WithDefault_ValueZero()
+        {
+            VarCommand cmd = new VarCommand();
+            Dictionary<string, int> variables = new Dictionary<string, int>();
+            int vCounter = 0;
+            string vName = "x";
+            string vValue = "";
+
+            // act
+            cmd.DeclareVariable(variables, ref vCounter, vName, vValue);
+
+            // Assert
+            Assert.IsTrue(variables.ContainsKey(vName));
+            Assert.AreEqual(0, variables[vName]);
+            Assert.AreEqual(1, vCounter);
+        }
+
+        /// <summary>
+        /// Tests the DeclareVariable method when the variable is already declared.
+        /// </summary>
+        [TestMethod]
+        public void DeclareVariable_With_WithGivenValue()
+        {
+            VarCommand cmd = new VarCommand();
+            Dictionary<string, int> variables = new Dictionary<string, int>();
+            int vCounter = 0;
+            string vName = "x";
+            string vValue = "10";
+
+            // act
+            cmd.DeclareVariable(variables, ref vCounter, vName, vValue);
+
+            // Assert
+            Assert.IsTrue(variables.ContainsKey(vName));
+            Assert.AreEqual(10, variables[vName]);
+            Assert.AreEqual(1, vCounter);
+        }
+
+
+        /// <summary>
+        /// Tests the Handle method when reassigning a variable to ensure the variable value is updated.
+        /// </summary>
+
+        [TestMethod]
+        public void Handle_UpdateValue_OnReassignment()
+        {
+            // Arrange
+            VarCommand cmd = new VarCommand();
+            Dictionary<string, int> variables = new Dictionary<string, int>() { { "x", 10 } };
+            int vCounter = 1;
+            string[] sampleParameters = { "x", "=", "x", "+", "1" };
+
+            // Act
+            cmd.Handle(sampleParameters, variables,  ref vCounter, true);
+
+            // Assert
+            Assert.AreEqual(11, variables["x"]);
+        }
+    }
+}
