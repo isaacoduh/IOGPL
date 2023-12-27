6814f519ae336b6fc107ae6e7ed033a45085baba
isaacoduh <ufuomaoduh@gmail.com>
2023-12-27 15:50:23 +0000

    Add Tests for Method and EndMethod Classes

--- IOGPL/EndMethodCommand.cs
+++ IOGPL/EndMethodCommand.cs
@@ -8,9 +8,24 @@ namespace IOGPL
 {
     public class EndMethodCommand
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EndMethodCommand"/> class.
+        /// </summary>
         public EndMethodCommand() { }
 
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EndMethodCommand"/> class with a reference to the <see cref="Proto"/> instance.
+        /// </summary>
+        /// <param name="p">The <see cref="Proto"/> instance.</param>
         public EndMethodCommand(Proto p) { }
+
+
+        /// <summary>
+        /// Handles the end method command.
+        /// </summary>
+        /// <param name="command">The command to handle.</param>
+        /// <param name="p">The <see cref="Proto"/> instance.</param>
         public void Handle(string command, Proto p)
         {
             // check if 
