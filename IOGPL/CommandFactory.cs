bea2e8d9401fc84190c119390360657de5cf927e
isaacoduh <ufuomaoduh@gmail.com>
2024-01-09 11:51:38 +0000

    Create Factory pattern Implementation

---
+++ IOGPL/CommandFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOGPL
+{
+    public class CommandFactory
+    {
+        public IProgramCommand CreateCommand(string commandName)
+        {
+            switch (commandName.ToLower())
+            {
+                case "if":
+                    return new IfCommand() as IProgramCommand;
+                    break;
+                case "loop":
+                    return new LoopCommand() as IProgramCommand;
+
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ IOGPL/IProgramCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOGPL
+{
+    public interface IProgramCommand
+    {
+        void Handle(string[] parts, Dictionary<string, int> variables, Proto proto, ref int variableCounter, BaseCanvas canvas, bool reassignment = false);
+    }
+}
